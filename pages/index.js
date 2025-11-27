caad1c1166861d6e5e528845cef92923eb86646c
Vinícius Pedro Damasceno Lima <vinicius.damasceno.dev@gmail.com>
2025-11-27 22:55:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Oi, esse é o início de uma jornada. 🌱🚀</h1>
+  return <h1>Oi, esse é o início de uma jornada. 🌱🚀</h1>;
+}
+
+function test() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("Identação errada");
 }
 
 export default Home;
